e69c30b9d6b7867e329f1c9e32c49a187dc8ae14
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2018-04-09 12:11:00 -0300

    Adds the fromBytes and fromDatagramPacket functions.

--- src/com/valkryst/VChat/message/Message.java
+++ src/com/valkryst/VChat/message/Message.java
@@ -1,11 +1,11 @@
 package com.valkryst.VChat.message;
 
 import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
+import java.net.DatagramPacket;
+import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class Message implements Serializable {
@@ -70,5 +70,71 @@ public class Message implements Serializable {
 
         // Retrieve Compressed Object Bytes from Byte Output Stream
         return byteArrayOutputStream.toByteArray();
+    }
+
+    /**
+     * Attempts to read a Message from a set of GZIPed bytes.
+     *
+     * @param data
+     *          The byte data.
+     *
+     * @return
+     *          The message.
+     *
+     * @throws IOException
+     *          If there's an IO error.
+     *
+     * @throws ClassNotFoundException
+     *          If the class of the serialized object, represented by the
+     *          input data, cannot be found.
+     */
+    public static Message fromBytes(final byte[] data) throws IOException, ClassNotFoundException {
+        // Create IO Streams
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        final GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream);
+        final ObjectInputStream objectInputStream = new ObjectInputStream(gzipInputStream);
+
+        // Create Shutdown Hook for IO Streams
+        final Thread shutdownCode = new Thread(() -> {
+            try {
+                objectInputStream.close();
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Runtime.getRuntime().addShutdownHook(shutdownCode);
+
+        // Convert Packet Bytes into Message
+        final Message message = (Message) objectInputStream.readObject();
+
+        // Remove Shutdown Hook and Close Streams
+        Runtime.getRuntime().removeShutdownHook(shutdownCode);
+        shutdownCode.run();
+
+        return message;
+    }
+
+    /**
+     * Attempts to read a Message from a DatagramPacket.
+     *
+     * @param packet
+     *          The packet.
+     *
+     * @return
+     *          The message, or null if a ClassNotFoundException occurs.
+     *
+     * @throws IOException
+     *          If there's an IO error.
+     */
+    public static Message fromDatagramPacket(final DatagramPacket packet) throws IOException {
+        try {
+            return fromBytes(packet.getData());
+        } catch (final ClassNotFoundException e) {
+            // Because the server isn't able to use the packet of the class, sent by the client, it logs
+            // the exception and ignores the packet.
+            LogManager.getLogger().error(packet.toString() + "\n\n" + e.getMessage());
+            return null;
+        }
     }
 }
