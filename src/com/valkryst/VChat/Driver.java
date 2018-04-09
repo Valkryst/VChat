f490c75c62c26604705e680cc88ba1711c22c5f6
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2018-04-09 12:18:32 -0300

    Updates driver.

--- src/com/valkryst/VChat/Driver.java
+++ src/com/valkryst/VChat/Driver.java
@@ -10,21 +10,17 @@ public class Driver {
         final MessageQueue serverMessageQueue = new MessageQueue();
         final ChatServer server = new ChatServer(21050, serverMessageQueue);
         server.start();
-
-        Thread.sleep(5000);
-        server.setContinueRunning(false);
         */
 
         final MessageQueue clientMessageQueue = new MessageQueue();
         final ChatClient client = new ChatClient("t.tiq.cc", 21050, clientMessageQueue);
         client.start();
 
-        for (int i = 0 ; i < 1 ; i++) {
+        for (int i = 0 ; i < 10 ; i++) {
             client.getMessageQueue().put(new Message(String.valueOf(i)));
         }
 
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         client.setContinueRunning(false);
-
     }
 }
